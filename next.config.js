eaa13cc52431f519b7aff986e4dbfc7ddfdc9ad6
Lê Xuân Huynh <huynhlx62@wru.vn>
2022-10-22 16:19:11 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,5 +2,4 @@ module.exports = {
   images: {
     domains: ["img.netpop.app"],
   },
-  swcMinify: false,
 };
--- src/pages/api/home.ts
+++ src/pages/api/home.ts
@@ -1,21 +1,32 @@
+// import { getHome } from '@/services/home';
+// import { NextRequest, NextResponse, NextMiddleware, NextFetchEvent } from 'next/server';
+// import fetchAdapter from '@vespaiach/axios-fetch-adapter';
+// import axios from '@/utils/axios';
+
+// export const middleware: NextMiddleware = async (req: NextRequest, ev: NextFetchEvent) => {
+//   axios.defaults.adapter = fetchAdapter;
+
+//   // Get the page from the url params
+//   const urlParams = new URLSearchParams(req.nextUrl.search);
+
+//   const page = urlParams.get("page");
+
+//   const data = await getHome(Number(page));
+
+//   return NextResponse.json(data);
+// };
+
+// export const config = {
+//   runtime: 'experimental-edge',
+// };
+
 import { getHome } from '@/services/home';
-import { NextRequest, NextResponse, NextMiddleware, NextFetchEvent } from 'next/server';
-import fetchAdapter from '@vespaiach/axios-fetch-adapter';
-import axios from '@/utils/axios';
+import { NextApiRequest, NextApiResponse } from "next";
 
-export const middleware: NextMiddleware = async (req: NextRequest, ev: NextFetchEvent) => {
-  axios.defaults.adapter = fetchAdapter;
-
-  // Get the page from the url params
-  const urlParams = new URLSearchParams(req.nextUrl.search);
-
-  const page = urlParams.get("page");
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { page } = req.query as { page: string; };
 
   const data = await getHome(Number(page));
 
-  return NextResponse.json(data);
-};
-
-export const config = {
-  runtime: 'experimental-edge',
+  res.send(data);
 };
